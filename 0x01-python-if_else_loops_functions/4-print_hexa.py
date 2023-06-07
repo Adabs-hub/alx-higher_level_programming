f3a453eec8af62b6c228745a0f138cdaf7c7e722
Adabs-hub <emmanueladabogo@gmail.com>
2023-06-06 19:38:29 -0700

    update and correction

--- 0x01-python-if_else_loops_functions/4-print_hexa.py
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 for i in range(99):
-    print(f"{i:d} = 0x{i:0x}")
+    print("{:d} = 0x{:0x}".format(i, i))
--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,7 +2,7 @@
 for i in range(90):
     if i % 10 > i / 10:
         if i != 89:
-            print(f"{i:02d}, ",end="")
+            print("{:02d}, ".format(i),end="")
         else:
-            print(f"{i:02d}")
+            print("{:02d}".format(i))
 
