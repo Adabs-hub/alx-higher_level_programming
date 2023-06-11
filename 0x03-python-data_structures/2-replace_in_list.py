bb21779fecc59525db4fb0b543da7ca19e1d5b5d
adabs-hub <emmanueladabogo@gmail.com>
2023-06-11 15:33:26 -0700

    task 2: replace list

---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def replace_in_list(my_list, idx, element):
+    if idx < len(my_list) and idx > -1:
+        my_list[idx] = element
+        return my_list
+    else:
+        return my_list
