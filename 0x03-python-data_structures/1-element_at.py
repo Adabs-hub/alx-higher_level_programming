16434847cb98b5d7508a068f0828d1f47ed0cbfc
adabs-hub <emmanueladabogo@gmail.com>
2023-06-11 15:23:24 -0700

    task 1: add element_MM

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def element_at(my_list, idx):
-    if idx < len(my_list) and adx >= 0:
+    if idx < len(my_list) and idx >= 0:
         return my_list[idx]
     else:
         return None
