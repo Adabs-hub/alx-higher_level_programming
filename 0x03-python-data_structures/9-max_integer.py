8b4f29ee9111d4033df3e19f065b8f3be3738fb5
adabs-hub <emmanueladabogo@gmail.com>
2023-06-11 20:27:21 -0700

    task 9: find biggest_MMMN

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 
 def max_integer(my_list=[]):
-    biggest = my_list[0]
 
-    if not my_list:
-        pass
+    if len(my_list) == 0:
+        return None
     else:
+        biggest = my_list[0]
         for i in range(len(my_list)):
             if biggest < my_list[i]:
                 biggest = my_list[i]
