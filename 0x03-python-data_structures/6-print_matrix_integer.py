e992d47470badbac4eb601b6a064da1e265a1663
adabs-hub <emmanueladabogo@gmail.com>
2023-06-11 17:56:48 -0700

    task 6: print matrix_M

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,10 +2,13 @@
 
 def print_matrix_integer(matrix=[[]]):
     m_len, l_len = len(matrix), 0
-    for i in range(m_len):
-        l_len = len(matrix[i])
-        for j in range(l_len):
-            if j < l_len - 1:
-                print("{:d}".format(matrix[i][j]), end=" ")
-            else:
-                print("{:d}".format(matrix[i][j]))
+    if m_len != 1 and len(matrix[0]) > 0:
+        for i in range(m_len):
+            l_len = len(matrix[i])
+            for j in range(l_len):
+                if j < l_len - 1:
+                    print("{:d}".format(matrix[i][j]), end=" ")
+                else:
+                    print("{:d}".format(matrix[i][j]))
+    else:
+        print()
---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+
+def add_tuple(tuple_a=(), tuple_b=()):
+    dk
