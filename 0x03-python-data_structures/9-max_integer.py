b55474a5b4f675d77114a222f8474e7434f57928
adabs-hub <emmanueladabogo@gmail.com>
2023-06-11 20:14:51 -0700

    task 9: find biggest_M

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -3,7 +3,10 @@
 def max_integer(my_list=[]):
     biggest = my_list[0]
 
-    for i in range(len(my_list)):
-        if biggest < my_list[i]:
-            biggest = my_list[i]
-    return biggest
+    if len(my_list) == 0:
+        return None
+    else:
+        for i in range(len(my_list)):
+            if biggest < my_list[i]:
+                biggest = my_list[i]
+        return biggest
