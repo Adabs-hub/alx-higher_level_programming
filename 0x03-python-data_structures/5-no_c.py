cd4d4b0578e3e2f6ccd2c4316f8086910ff4df3f
adabs-hub <emmanueladabogo@gmail.com>
2023-06-11 17:01:25 -0700

    task 5: remove c char

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,2 +1,8 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
+def no_c(my_string):
+    new_string = ""
+    for i in range(len(my_string)):
+        if my_string[i] != "c" and my_string[i] != "C":
+            new_string = f"{new_string}{my_string[i]}"
+    return new_string
