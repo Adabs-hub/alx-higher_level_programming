0411e90070aa3bdfea97d69348a6c909190512b1
adabs-hub <emmanueladabogo@gmail.com>
2023-06-11 19:22:03 -0700

    task 7: add tuple at index

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,4 +1,17 @@
 #!/usr/bin/python3
 
 def add_tuple(tuple_a=(), tuple_b=()):
-    dk
+    num1, num2 = 0, 0
+    tuple_new = ()
+    for i in range(2):
+        if i > (len(tuple_a) - 1):
+            num1 = 0
+        else:
+            num1 = tuple_a[i]
+        if i > (len(tuple_b) - 1):
+            num2 = 0
+        else:
+            num2 = tuple_b[i]
+        tuple_new += (num1 + num2),
+
+    return tuple_new
