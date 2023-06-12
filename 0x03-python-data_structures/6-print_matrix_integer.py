2e395495d80cb3f7e830d0b8389ae957663c8838
adabs-hub <emmanueladabogo@gmail.com>
2023-06-11 17:34:46 -0700

    task 6: print matrix

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def print_matrix_integer(matrix=[[]]):
+    m_len, l_len = len(matrix), 0
+    for i in range(m_len):
+        l_len = len(matrix[i])
+        for j in range(l_len):
+            if j < l_len - 1:
+                print("{:d}".format(matrix[i][j]), end=" ")
+            else:
+                print("{:d}".format(matrix[i][j]))
