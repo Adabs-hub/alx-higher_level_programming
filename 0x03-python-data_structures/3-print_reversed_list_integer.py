b7ad8bbf8a0091b2b4a38b3d707fd0268a80472c
adabs-hub <emmanueladabogo@gmail.com>
2023-06-11 18:33:14 -0700

    task 3: print reverse_MMMMM

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 
 def print_reversed_list_integer(my_list=[]):
+
     size = len(my_list)
+
     if not my_list:
-        return None
+        pass
     else:
-        my_list.reverse()
         for i in range(size):
-            print("{:d}".format(my_list[i]))
+            print("{:d}".format(my_list[size - 1 - i]))
