867386cab3051a969d66f1d6b6e86c07b280cb94
adabs-hub <emmanueladabogo@gmail.com>
2023-06-11 21:14:26 -0700

    task 10: is divisible

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def divisible_by_2(my_list=[]):
+    if not my_list:
+        pass
+    else:
+        new_list = []
+        for i in range(len(my_list)):
+            if (my_list[i] % 2) == 0:
+                new_list.append(1)
+            else:
+                new_list.append(0)
+        return new_list
