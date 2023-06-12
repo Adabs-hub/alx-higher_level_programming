30376de83823c5261258d4c045ae3ffe5cbd7375
adabs-hub <emmanueladabogo@gmail.com>
2023-06-11 18:01:34 -0700

    task 6: print matrix_MM

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,7 +2,7 @@
 
 def print_matrix_integer(matrix=[[]]):
     m_len, l_len = len(matrix), 0
-    if m_len != 1 and len(matrix[0]) > 0:
+    if m_len != 1 and len(matrix[0]) != 0:
         for i in range(m_len):
             l_len = len(matrix[i])
             for j in range(l_len):
