485bf7a88871e60bf2101cd3684539ced70c23a9
adabs-hub <emmanueladabogo@gmail.com>
2023-06-14 16:40:22 -0700

    task 0: update_M

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -2,9 +2,9 @@
 
 def square_matrix_simple(matrix=[]):
 
-	new_matrix = [list(row) for row in matrix]
-	for i in range(len(matrix)):
-		for j in range(len(matrix[i])):
-			new_matrix[i][j] = matrix[i][j]**2
+    new_matrix = [list(row) for row in matrix]
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+            new_matrix[i][j] = matrix[i][j]**2
 
-	return new_matrix
+    return new_matrix
