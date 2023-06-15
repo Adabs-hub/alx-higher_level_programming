667f9cf8c470c250696fe3085ee41369ca325332
adabs-hub <emmanueladabogo@gmail.com>
2023-06-14 19:51:57 -0700

    task 10: finised_M

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,11 +1,7 @@
 #!/usr/bin/python3
 
 def best_score(a_dictionary):
-    max_value = 0
     if not a_dictionary:
         return None
-    for key in a_dictionary:
-        if max_value < a_dictionary[key]:
-            max_key = key
 
-    return max_key
+    return (max(a_dictionary, key=a_dictionary.get))
