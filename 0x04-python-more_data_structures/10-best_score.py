b3314ecbdaecab7b07cd35e8024ad86a534d5b4d
adabs-hub <emmanueladabogo@gmail.com>
2023-06-14 19:48:05 -0700

    task 10: finised

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 
 def best_score(a_dictionary):
-    max_key = 0
+    max_value = 0
+    if not a_dictionary:
+        return None
     for key in a_dictionary:
-        if max_key < key:
+        if max_value < a_dictionary[key]:
             max_key = key
 
     return max_key
