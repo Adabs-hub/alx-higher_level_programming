64f3400986d4ae574ab4ee3977b1e676aac80816
adabs-hub <emmanueladabogo@gmail.com>
2023-06-14 19:24:16 -0700

    task 9: *2

---
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def multiply_by_2(a_dictionary):
+    new_dictionary = a_dictionary.copy()
+    for key in a_dictionary:
+        new_dictionary[key] *= 2
+
+    return new_dictionary
