7690cd40ed89a2d80f7efc9ec8d58c50b701b445
adabs-hub <emmanueladabogo@gmail.com>
2023-06-14 19:08:07 -0700

    task 8 update

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 
 def simple_delete(a_dictionary, key=""):
-    del a_dictionary[key]
+    for k, v in a_dictionary:
+        if k == key:
+            del a_dictionary[key]
+
     return a_dictionary
