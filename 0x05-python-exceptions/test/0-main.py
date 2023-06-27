b52b74dc599176a8f1d752885d0bda69aa826084
adabs-hub <emmanueladabogo@gmail.com>
2023-06-26 18:37:37 -0700

    test file: task 0

---
+++ 0x05-python-exceptions/test/0-main.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import sys
+
+sys.path.insert(0, '/home/alx-high_level_programming/0x05-python-exceptions')
+safe_print_list = __import__('0-safe_print_list').safe_print_list
+
+my_list = [1, 2, 3, 4, 5]
+
+nb_print = safe_print_list(my_list, 2)
+print("nb_print: {:d}".format(nb_print))
+nb_print = safe_print_list(my_list, len(my_list))
+print("nb_print: {:d}".format(nb_print))
+nb_print = safe_print_list(my_list, len(my_list) + 2)
+print("nb_print: {:d}".format(nb_print))
