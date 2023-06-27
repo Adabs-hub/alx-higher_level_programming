9cf03a8273060858c88325bcda769092716d7441
adabs-hub <emmanueladabogo@gmail.com>
2023-06-26 19:28:07 -0700

    test file: task one complete

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 
-def safe_print_int(value):
+def safe_print_integer(value):
     try:
-        print("{d:}".format(value))
-    except KeyError:
-            return (False)
-
-    return (True)
+        print("{:d}".format(value))
+        return True
+    except ValueError:
+        return False
---
+++ 0x05-python-exceptions/test/1-main.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+import sys
+
+sys.path.insert(0, '/home/alx-high_level_programming/0x05-python-exceptions')
+
+safe_print_integer = __import__('1-safe_print_integer').safe_print_integer
+
+value = 89
+has_been_print = safe_print_integer(value)
+if not has_been_print:
+    print("{} is not an integer".format(value))
+
+value = -89
+has_been_print = safe_print_integer(value)
+if not has_been_print:
+    print("{} is not an integer".format(value))
+
+value = "School"
+has_been_print = safe_print_integer(value)
+if not has_been_print:
+    print("{} is not an integer".format(value))
