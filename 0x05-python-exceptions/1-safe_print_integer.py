eca3111004309b6a82e8725f36c58b0f5f1deb55
adabs-hub <emmanueladabogo@gmail.com>
2023-06-26 19:42:12 -0700

    update

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -3,7 +3,6 @@
 def safe_print_integer(value):
     try:
         print("{:d}".format(value))
-    except ValueError:
-        return False
-    else:
         return True
+    except Exception:
+        return False
