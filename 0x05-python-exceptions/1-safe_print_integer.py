a775b65bce4bb46c732286febd721ac8ba2b779c
adabs-hub <emmanueladabogo@gmail.com>
2023-06-26 21:04:48 -0700

    task 1: include extra exception

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 
+
 def safe_print_integer(value):
     try:
         print("{:d}".format(value))
-        return True
-    except Exception:
-        return False
+        return (True)
+    except (TypeError, ValueError):
+        return (False)
