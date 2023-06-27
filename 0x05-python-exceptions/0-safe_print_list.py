11817296524365134e9892e6eaafd4baa8c23a70
adabs-hub <emmanueladabogo@gmail.com>
2023-06-26 18:41:52 -0700

    update task 0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
 
 def safe_print_list(my_list=[], x=0):
+    count = 0
     for i in range(x):
         try:
             print(my_list[i], end="")
+            count += 1
         except IndexError:
             print()
-            return (i)
+            return (count)
 
     print()
-    return (i+1)
+    return (count)
