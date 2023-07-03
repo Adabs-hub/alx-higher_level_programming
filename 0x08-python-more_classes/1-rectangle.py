5fe77ad7d88a6a4fde09043601c9af0cb5629de1
adabs-hub <emmanueladabogo@gmail.com>
2023-07-03 15:33:23 -0700

    task 1

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -23,10 +23,10 @@ class Rectangle:
     @width.setter
     def width(self, value):
         """ set width """
-        try:
-            self.__width = value
-        except TypeError:
-            print("width must be an integer")
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+
+        self.__width = value
         if value < 0:
             raise ValueError("width must be >=0")
         return self.__width
@@ -39,10 +39,10 @@ class Rectangle:
     @height.setter
     def height(self, value):
         """ set height """
-        try:
-            self.__height = value
-        except TypeError:
-            print("height must be an integer")
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+
+        self.__height = value
         if value < 0:
             raise ValueError("height must be >= 0")
         return self.__height
