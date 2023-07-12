b60d6c909284dd135ceb8dbf1346adf6b94d5efe
adabs-hub <emmanueladabogo@gmail.com>
2023-07-12 07:35:41 -0700

    task 0: readfile MM

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python
+""" Read the content of a file """
+
 
 def read_file(filename=""):
-    """ Read the content of a file """
+    """ Read the content of a file in utf-8 encoding """
     with open(filename, "r", encoding="utf-8") as _file:
         read_data = _file.read()
         print(read_data, end="")
