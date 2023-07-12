892ff80705b3e3d6fba148c79d55c14b1d450446
adabs-hub <emmanueladabogo@gmail.com>
2023-07-12 07:15:16 -0700

    task 0: read content of a file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
 def read_file(filename=""):
+    """ Read the content of a file """
     with open(filename, "r", encoding="utf-8") as _file:
         read_data = _file.read()
         print(read_data, end="")
