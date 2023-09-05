b8661c6b2eb46bf333db28c7c0fa27158005f16f
Adabs-hub <emmanueladabogo@gmail.com>
2023-09-04 19:36:41 -0700

    update task 1

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,33 +1,35 @@
 #!/usr/bin/python3
 
-""" Class Rectangle """
+"""Class Rectangle."""
 
 
 
 class Rectangle:
     """
-    initialize rectangle class
-    set width and height private property
-    only assigns with and height when they are greater 0
+    initialize rectangle.
+
+    private instance property: width (int).
+    private instance property: height (int).
     """
+
     def __init__(self, width=0, height=0):
-        """class constructor or instance"""
+        """Class constructor or instance."""
         self.width = width
         self.height = height
 
     @property
     def width(self):
-        """returns the width of the rectangle"""
+        """Returns the width of the rectangle."""
         return self.__width
 
     @property
     def height(self):
-        """returns the height of the rectangle"""
+        """Returns the height of the rectangle."""
         return self.__height
 
     @width.setter
     def width(self, value):
-        """sets the width of the rectangle"""
+        """Set the width of the rectangle."""
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
@@ -38,7 +40,7 @@ class Rectangle:
 
     @height.setter
     def height(self, value):
-        """sets the height of the rectangle"""
+        """Set the height of the rectangle."""
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
