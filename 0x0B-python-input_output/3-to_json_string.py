49ef410b288c7da2ea6c11ce67656b8b73300668
Adabs-hub <emmanueladabogo@gmail.com>
2023-09-12 18:57:10 -0700

    object as json

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""JSON representation of strigng"""
+import json
+
+def to_json_string(my_obj):
+    """
+    to_json_string
+
+    -returns the json representation of an object
+    """
+    return json.dumps(my_obj)
