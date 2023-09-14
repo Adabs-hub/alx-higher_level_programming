cebe7666990a89f7aab18d0a22987fd3a5a284ce
Adabs-hub <emmanueladabogo@gmail.com>
2023-09-13 18:21:24 -0700

    task 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -3,6 +3,7 @@ import sys
 import os
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+"""Load and save file"""
 
 args = sys.argv
 del args[0]
