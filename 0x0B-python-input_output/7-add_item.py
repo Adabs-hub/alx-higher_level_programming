90cdcb9d07453afbb2a5ea2f8c53ef3cc31be10f
Adabs-hub <emmanueladabogo@gmail.com>
2023-09-13 18:19:41 -0700

    task 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -8,11 +8,13 @@ args = sys.argv
 del args[0]
 filename = "add_item.json"
 my_list = []
+
 if os.path.exists(filename):
     load = load_from_json_file(filename)
     if len(args) > 0:
         my_list.extend(load)
         my_list.extend(args)
         save_to_json_file(my_list, filename)
+
 else:
     save_to_json_file(my_list, filename)
