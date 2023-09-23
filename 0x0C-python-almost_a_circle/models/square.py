61abc7fbe0f209ec04bb86c7a6dfc23ce8ab4f97
Adabs-hub <emmanueladabogo@gmail.com>
2023-09-22 17:52:14 -0700

    update square

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -54,7 +54,7 @@ class Square(Rectangle):
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
-            raise ValueError("width must be >= 0")
+            raise ValueError("width must be > 0")
         else:
             self.__width = value
             self.__height = value
